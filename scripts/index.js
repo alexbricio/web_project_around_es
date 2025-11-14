dd77355ebf618d79cb3093822a20440bf70c52a1
AlexBricio <amoralesbricio@gmail.com>
2025-11-13 22:12:44 -0600

    Seguna parte etapa 1

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,38 @@ const initialCards = [
 initialCards.forEach(function (item) {
   console.log(item);
 });
+
+const editar_perfil = document.querySelector(".profile__edit-button");
+const modal_popup = document.querySelector("#edit-popup");
+const cerrar = modal_popup.querySelector(".popup__close");
+const nombre_perfil = document.querySelector(".profile__title");
+const descripcion = document.querySelector(".profile__description");
+const nombre_input = modal_popup.querySelector(".popup__input_type_name");
+const descripcion_input = modal_popup.querySelector(
+  ".popup__input_type_description"
+);
+
+function openModal(element) {
+  element.classList.add("popup_is-opened");
+}
+function closeModal(element) {
+  element.classList.remove("popup_is-opened");
+}
+
+editar_perfil.addEventListener("click", function () {
+  handleOpenEditModal();
+});
+
+cerrar.addEventListener("click", function () {
+  closeModal(modal_popup);
+});
+
+function fillProfileForm() {
+  nombre_input.value = nombre_perfil.textContent;
+  descripcion_input.value = descripcion.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(modal_popup);
+}
