dea580a9abdb4708be87cb5e7993521ecf1dbdc5
AlexBricio <amoralesbricio@gmail.com>
2025-12-14 22:40:09 -0600

    feat: agrega parte 1

--- scripts/index.js
+++ scripts/index.js
@@ -29,14 +29,11 @@ const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
-
 const cardContainer = document.querySelector(".cards__list");
 const imagePopup = document.querySelector("#image-popup");
 const popupImage = imagePopup.querySelector(".popup__image");
 const popupCaption = imagePopup.querySelector(".popup__caption");
 const popupButton = imagePopup.querySelector(".popup__close");
-
-
 
 function getCardElement(
   name = "Sin título",
@@ -52,29 +49,27 @@ function getCardElement(
   cardImage.alt = name;
   cardTitle.textContent = name;
 
-  likeButton.addEventListener("click", function(){
+  likeButton.addEventListener("click", function () {
     likeButton.classList.toggle("card__like-button_is-active");
   });
 
-  deleteButton.addEventListener("click", function(){
+  deleteButton.addEventListener("click", function () {
     cardTempleteClon.remove();
   });
 
-  cardImage.addEventListener("click", function(){
+  cardImage.addEventListener("click", function () {
     popupCaption.textContent = name;
     popupImage.src = link;
     popupImage.alt = name;
     openModal(imagePopup);
-    
   });
 
-
- return cardTempleteClon;
+  return cardTempleteClon;
 }
 
- popupButton.addEventListener("click", function(){
-    closeModal(imagePopup);
-  });
+popupButton.addEventListener("click", function () {
+  closeModal(imagePopup);
+});
 
 function renderCard(name, link, container) {
   const cardElement = getCardElement(name, link);
@@ -109,6 +104,7 @@ function fillProfileForm() {
 }
 
 function handleOpenEditModal() {
+  updateButton();
   fillProfileForm();
   openModal(modal_popup);
 }
@@ -134,7 +130,6 @@ function handleProfileFormSubmit(evt) {
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
 
-
 const addButton = document.querySelector(".profile__add-button");
 const newCardPop = document.querySelector("#new-card-popup");
 const closeNewCard = newCardPop.querySelector(".popup__close");
@@ -142,11 +137,11 @@ const popupForm = document.querySelector("#new-card-form");
 const inputName = popupForm.querySelector(".popup__input_type_card-name");
 const inputUrl = popupForm.querySelector(".popup__input_type_url");
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
   openModal(newCardPop);
 });
 
-closeNewCard.addEventListener("click", function(){
+closeNewCard.addEventListener("click", function () {
   closeModal(newCardPop);
 });
 
@@ -163,7 +158,54 @@ function handleCardFormSubmit(evt) {
   popupForm.reset();
 
   closeModal(newCardPop);
-  
 }
 
-popupForm.addEventListener("submit", handleCardFormSubmit);
+const form = document.getElementById("edit-profile-form");
+const profileNameInput = form.querySelector("#profile-name");
+const profileDescriptionInput = form.querySelector("#profile-description");
+const buttonProfile = form.querySelector(".popup__button");
+
+function updateButton() {
+  if (
+    profileNameInput.checkValidity() &&
+    profileDescriptionInput.checkValidity()
+  ) {
+    buttonProfile.disabled = false;
+  } else {
+    buttonProfile.disabled = true;
+  }
+}
+
+profileNameInput.addEventListener("input", function () {
+  if (!profileNameInput.validity.valid) {
+    showInputError(profileNameInput);
+  } else {
+    hideInputError(profileNameInput);
+  }
+  updateButton();
+});
+
+profileDescriptionInput.addEventListener("input", function () {
+  if (!profileDescriptionInput.validity.valid) {
+    showInputError(profileDescriptionInput);
+  } else {
+    hideInputError(profileDescriptionInput);
+  }
+  updateButton();
+});
+
+function showInputError(inputElement) {
+  const errorElement = form.querySelector(`.${inputElement.id}-input-error`);
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add("popup__input-error_active");
+}
+
+function hideInputError(inputElement) {
+  const errorElement = form.querySelector(`.${inputElement.id}-input-error`);
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.textContent = "";
+  errorElement.classList.remove("popup__input-error_active");
+}
+
+updateButton();
